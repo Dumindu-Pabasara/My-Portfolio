40f940e220f3612de62529d0f9f9b471f002de38
Dumindu_Ekanayake <duminduekanayake6@gmail.com>
2025-10-23 01:02:34 +0530

    update

---
+++ Portfolio/src/assets/mywork_data.js
@@ -0,0 +1,48 @@
+
+import project6_img from '../assets/project_6.svg'
+import HotelWebsite from '../assets/hotelwebsite.png'
+import driver from '../assets/driver.jpg'
+import client from '../assets/client.jpeg'
+import music from '../assets/music.jpeg'
+import travel from '../assets/travel.jpeg'
+
+const mywork_data = [
+    {
+        w_no: 1,
+        w_name: "Web Design",
+        w_img: HotelWebsite,
+        w_link: "https://www.figma.com/design/eA3c73YmC1kQUGcbMSi5OE/Hotel-Website?node-id=0-1&t=NgopsXff33g7E2yt-1"  // 🔗 add your project URL here
+    },
+    {
+        w_no: 2,
+        w_name: "Web Design",
+        w_img: driver,
+        w_link: "https://www.figma.com/design/0AnUkWiNInVMccNEkfJXHl/MUVE-Driver?node-id=0-1&t=YRjhl5bRqh3gTaS0-1"
+    },
+    {
+        w_no: 3,
+        w_name: "Web Design",
+        w_img: client,
+        w_link: "https://www.figma.com/design/GmeGXTpHFZWZ5S2QOyQmq6/MUVE?node-id=108-837&t=MgrKcMjfrV7UYruH-1"
+    },
+    {
+        w_no: 4,
+        w_name: "Web Design",
+        w_img: music,
+        w_link: "https://www.figma.com/design/cZJ0Bai9loYctVpkLoD6aJ/Music-App?node-id=0-1&t=DbqJiiLYLudassxO-1"
+    },
+    {
+        w_no: 5,
+        w_name: "Web Design",
+        w_img: travel,
+        w_link: "https://www.figma.com/design/Am8TQEZEekTjOEmzWPvMNA/Capston?node-id=0-1&t=3OIAFre5GKTBtEfs-1"
+    },
+    {
+        w_no: 6,
+        w_name: "Web Design",
+        w_img: project6_img,
+        w_link: "https://www.behance.net/duminduekanaya"
+    },
+]
+
+export default mywork_data;
---
+++ Portfolio/src/assets/mywork_data2.js
@@ -0,0 +1,49 @@
+
+import project6_img from '../assets/project_6.svg'
+import HotelWebsite from '../assets/hotelwebsite.png'
+
+import travel from '../assets/travel.jpeg'
+import srilanka from '../assets/srilanka.png'
+import student from '../assets/student.jpeg'
+import music from '../assets/music.jpeg'
+
+const mywork_data = [
+    {
+        w_no: 1,
+        w_name: "Web Design",
+        w_img: HotelWebsite,
+        w_link: "https://github.com/Dumindu-Pabasara/Hotel_Management_System.git"  // 🔗 add your project URL here
+    },
+    {
+        w_no: 2,
+        w_name: "Web Design",
+        w_img: travel,
+        w_link: "https://github.com/Dumindu-Pabasara/capston_project.git"
+    },
+    {
+        w_no: 3,
+        w_name: "Web Design",
+        w_img: srilanka,
+        w_link: "https://github.com/Dumindu-Pabasara/Travel_Website.git"
+    },
+    {
+        w_no: 4,
+        w_name: "Web Design",
+        w_img: student,
+        w_link: "https://github.com/Dumindu-Pabasara/Student-Management-System.git"
+    },
+    {
+        w_no: 5,
+        w_name: "Web Design",
+        w_img: music,
+        w_link: "https://www.figma.com/design/Am8TQEZEekTjOEmzWPvMNA/Capston?node-id=0-1&t=3OIAFre5GKTBtEfs-1"
+    },
+    {
+        w_no: 6,
+        w_name: "Web Design",
+        w_img: project6_img,
+        w_link: "https://github.com/Dumindu-Pabasara"
+    },
+]
+
+export default mywork_data;
